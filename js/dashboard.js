db7e1244c71a3b4d37239150ddb431397198ab91
Farouq960 <aqil.farouq99@gmail.com>
2022-07-16 15:07:49 +0800

    Update: 3 graph to 5 graph

--- js/dashboard.js
+++ js/dashboard.js
@@ -43,17 +43,19 @@ Promise.all([
     })
 ]).then(function(loadData) {
     createChoropleth(loadData[0])
-    createLine()
+    createLineDeath()
+    createLineVaccine()
+    createScatter()
 })
 
 function createChoropleth(topo) {
 
     var svg = d3.select("#choroplethChart"),
-        width = +svg.attr("width"),
+        width = (+svg.attr("width").replace("%","") /100)* screen.width,
         height = +svg.attr("height");
     var projection = d3.geoMercator()
-        .scale(2000) // adjust size map
-        .center([119.6, 0])
+        .scale(2100) // adjust size map
+        .center([121.5, -2])
         .translate([width, height]);
     var path = d3.geoPath().projection(projection);
 
@@ -126,7 +128,8 @@ function createChoropleth(topo) {
         console.log(datas[i].State)
         console.log(d.properties.name)
 
-        updateLine(lineData,d.properties.name,colorScale(d.total))
+        updateLineDeath(lineData,d.properties.name,colorScale(d.total))
+        updateLineVaccine(lineData,d.properties.name,colorScale(d.total))
     }
 
 
@@ -152,18 +155,145 @@ function createChoropleth(topo) {
         .on("click", mouseClick);
 }
 
-function createLine() {
-    var svg = d3.select("#lineChart"),
+function createLineDeath() {
+    var svg = d3.select("#lineChartDeath"),
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
-    var maxY = d3.max(lineAllData, function(d) { return d3.max([+d.Vaccine, +d.Death]) });
-    var minY = d3.min(lineAllData, function(d) { return d3.min([+d.Vaccine, +d.Death]) });
+    var maxY = d3.max(lineAllData, function(d) { return d3.max([+d.Death]) });
+    var minY = d3.min(lineAllData, function(d) { return d3.min([+d.Death]) });
 
     domain = d3.extent(lineAllData, function(d) { return d.Date })
 
     let y = d3.scaleLinear()
-        .domain([minY - 1000, maxY + 1000])
+        .domain([0, maxY + 1000])
+        .range([(height * 0.8), 25]);
+
+    let x = d3.scaleTime()
+        .domain(domain)
+        .range([0, (width * 0.8)]);
+
+    let yAxis = d3.axisLeft(y).ticks(7);
+    let xAxis = d3.axisBottom(x).ticks(5).tickFormat(formatDate);
+
+    svg.append('svg')
+        .attr('height', height - 30)
+        .attr('width', width - 100);
+
+    var chartGroup = svg.append('g')
+        .attr('transform', 'translate(50,50)');
+
+    var lineDeath = d3.line()
+        .x(function(d) { return x(d.Date); })
+        .y(function(d) { return y(+d.Death); })
+
+    chartGroup.append('path').attr('d', lineDeath(lineAllData)).attr('id', 'lineDeath')
+    chartGroup.append('g').attr('class', 'x axis').attr('transform', 'translate(0,320)').call(xAxis)
+    chartGroup.append('g').attr('class', 'y axis').attr('transform', 'translate(0,0)').call(yAxis)
+
+    chartGroup.select('#lineDeath').attr('fill', 'none').attr('stroke', 'red')
+
+    chartGroup.append('g')
+        .attr('id', 'dotsDeath')
+        .selectAll("dot")
+        .data(lineAllData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(d.Date); })
+        .attr("cy", function(d) { return y(+d.Death); })
+        .attr("r", 2)
+        .style("fill", "black")
+        .on("mouseover", mouseOverDeath)
+        .on("mouseleave", mouseLeave);
+
+    var tooltip = d3.select("#dtip")
+        .append("div")
+        .attr('id', 'tipLine')
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .style("position", "absolute")
+
+    function mouseOverDeath(d) {
+        tooltip
+            .transition()
+            .duration(10)
+            .style("opacity", 1);
+        tooltip
+            .html("Date: " + d.Date + "<br>Total Death: " + d.Death)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 90) + "px")
+    }
+
+    function mouseLeave(d) {
+        tooltip
+            .transition()
+            .duration(100)
+            .style("opacity", 0)
+            .style('pointer-events', 'none');
+    }
+}
+
+function updateLineDeath(lineData, country, color){
+    var svg = d3.select("#lineChartDeath"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+        
+    var maxY = d3.max(lineData, function(d) { return d3.max([+d.Death]) });
+    var minY = d3.min(lineData, function(d) { return d3.min([+d.Death]) });
+
+    domain = d3.extent(lineData, function(d) { return d.Date })
+
+    let y = d3.scaleLinear()
+        .domain([minY, maxY + 10])
+        .range([(height * 0.8), 25]);
+
+    let x = d3.scaleTime()
+        .domain(domain)
+        .range([0, (width * 0.8)]);
+
+    let yAxis = d3.axisLeft(y).ticks(7);
+    let xAxis = d3.axisBottom(x).ticks(5).tickFormat(formatDate);
+
+    var lineVacc = d3.line()
+        .x(function(d) { return x(d.Date); })
+        .y(function(d) { return y(+d.Vaccine); })
+    var lineDeath = d3.line()
+        .x(function(d) { return x(d.Date); })
+        .y(function(d) { return y(+d.Death); })
+
+    var chartGroup = svg.select('g')
+
+    chartGroup.select('#lineDeath').transition().duration(750).attr('d', lineDeath(lineData)).attr('fill', 'none')
+    chartGroup.select('g.x.axis').call(xAxis)
+    chartGroup.select('g.y.axis').call(yAxis)
+    
+    chartGroup.select('#dotsDeath')
+        .selectAll("circle")
+        .data(lineData)
+        .transition()
+        .duration(750)
+        .attr('fill', 'none')
+        .attr("cy", function(d) { return y(+d.Death); })
+
+}
+
+function createLineVaccine() {
+    var svg = d3.select("#lineChartVaccine"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    var maxY = d3.max(lineAllData, function(d) { return d3.max([+d.Vaccine]) });
+    var minY = d3.min(lineAllData, function(d) { return d3.min([+d.Vaccine]) });
+
+    domain = d3.extent(lineAllData, function(d) { return d.Date })
+
+    let y = d3.scaleLinear()
+        .domain([minY - 1000, maxY + 100000])
         .range([(height * 0.8), 25]);
 
     let x = d3.scaleTime()
@@ -183,17 +313,12 @@ function createLine() {
     var lineVacc = d3.line()
         .x(function(d) { return x(d.Date); })
         .y(function(d) { return y(+d.Vaccine); })
-    var lineDeath = d3.line()
-        .x(function(d) { return x(d.Date); })
-        .y(function(d) { return y(+d.Death); })
 
     chartGroup.append('path').attr('d', lineVacc(lineAllData)).attr('id', 'lineVacc')
-    chartGroup.append('path').attr('d', lineDeath(lineAllData)).attr('id', 'lineDeath')
     chartGroup.append('g').attr('class', 'x axis').attr('transform', 'translate(0,320)').call(xAxis)
     chartGroup.append('g').attr('class', 'y axis').attr('transform', 'translate(0,0)').call(yAxis)
 
     chartGroup.select('#lineVacc').attr('fill', 'none').attr('stroke', 'green')
-    chartGroup.select('#lineDeath').attr('fill', 'none').attr('stroke', 'red')
 
     chartGroup.append('g')
         .attr('id', 'dotsVacc')
@@ -206,19 +331,6 @@ function createLine() {
         .attr("r", 2)
         .style("fill", "blue")
         .on("mouseover", mouseOverVacc)
-        .on("mouseleave", mouseLeave);
-
-    chartGroup.append('g')
-        .attr('id', 'dotsDeath')
-        .selectAll("dot")
-        .data(lineAllData)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d.Date); })
-        .attr("cy", function(d) { return y(+d.Death); })
-        .attr("r", 4)
-        .style("fill", "black")
-        .on("mouseover", mouseOverDeath)
         .on("mouseleave", mouseLeave);
 
     var tooltip = d3.select("#dtip")
@@ -244,17 +356,6 @@ function createLine() {
             .style("top", (d3.event.pageY - 90) + "px")
     }
 
-    function mouseOverDeath(d) {
-        tooltip
-            .transition()
-            .duration(10)
-            .style("opacity", 1);
-        tooltip
-            .html("Date: " + d.Date + "<br>Total Death: " + d.Death)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 90) + "px")
-    }
-
     function mouseLeave(d) {
         tooltip
             .transition()
@@ -264,18 +365,18 @@ function createLine() {
     }
 }
 
-function updateLine(lineData, country, color){
-    var svg = d3.select("#lineChart"),
+function updateLineVaccine(lineData, country, color){
+    var svg = d3.select("#lineChartVaccine"),
         width = +svg.attr("width"),
         height = +svg.attr("height");
-        
-    var maxY = d3.max(lineData, function(d) { return d3.max([+d.Vaccine, +d.Death]) });
-    var minY = d3.min(lineData, function(d) { return d3.min([+d.Vaccine, +d.Death]) });
+
+    var maxY = d3.max(lineData, function(d) { return d3.max([+d.Vaccine]) });
+    var minY = d3.min(lineData, function(d) { return d3.min([+d.Vaccine]) });
 
     domain = d3.extent(lineData, function(d) { return d.Date })
 
     let y = d3.scaleLinear()
-        .domain([minY - 1000, maxY + 1000])
+        .domain([minY, maxY + 100000])
         .range([(height * 0.8), 25]);
 
     let x = d3.scaleTime()
@@ -288,31 +389,94 @@ function updateLine(lineData, country, color){
     var lineVacc = d3.line()
         .x(function(d) { return x(d.Date); })
         .y(function(d) { return y(+d.Vaccine); })
-    var lineDeath = d3.line()
-        .x(function(d) { return x(d.Date); })
-        .y(function(d) { return y(+d.Death); })
 
     var chartGroup = svg.select('g')
 
     chartGroup.select('#lineVacc').transition().duration(750).attr('d', lineVacc(lineData)).attr('fill', 'none')
-    chartGroup.select('#lineDeath').transition().duration(750).attr('d', lineDeath(lineData)).attr('fill', 'none')
     chartGroup.select('g.x.axis').call(xAxis)
     chartGroup.select('g.y.axis').call(yAxis)
-
+    
     chartGroup.select('#dotsVacc')
         .selectAll("circle")
         .data(lineData)
         .transition()
         .duration(750)
         .attr('fill', 'none')
-        .attr("cy", function(d) { return y(+d.Vaccine); });
-    
-    chartGroup.select('#dotsDeath')
-        .selectAll("circle")
-        .data(lineData)
-        .transition()
-        .duration(750)
-        .attr('fill', 'none')
-        .attr("cy", function(d) { return y(+d.Death); })
+        .attr("cy", function(d) { return y(+d.Vaccine); })
 
+}
+
+function createScatter(){
+    var svg = d3.select("#scatterChart"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+    
+    var yDomain = d3.extent(lineAllData, function(d) { return d3.max([+d.Vaccine])});
+    var xDomain = d3.extent(lineAllData, function(d) { return d3.max([+d.Death])});
+
+    let y = d3.scaleLinear()
+        .domain(yDomain)
+        .range([(height * 0.8), 25]);
+
+    let x = d3.scaleLinear()
+        .domain(xDomain)
+        .range([0, (width * 0.8)]);
+    
+    let yAxis = d3.axisLeft(y).ticks(7);
+    let xAxis = d3.axisBottom(x).ticks(5);
+
+    svg.append('svg')
+        .attr('height', height - 30)
+        .attr('width', width - 100);
+
+    var chartGroup = svg.append('g')
+        .attr('transform', 'translate(50,50)');
+
+    chartGroup.append('g').attr('class', 'x axis').attr('transform', 'translate(0,320)').call(xAxis);
+    chartGroup.append('g').attr('class', 'y axis').attr('transform', 'translate(0,0)').call(yAxis);
+
+    chartGroup.append('g')
+        .attr('id', 'dotsRel')
+        .selectAll("dot")
+        .data(lineAllData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(+d.Death); })
+        .attr("cy", function(d) { return y(+d.Vaccine); })
+        .attr("r", 4)
+        .style("fill", "black")
+        .on("mouseover", mouseOver)
+        .on("mouseleave", mouseLeave);
+
+    var tooltip = d3.select("#dtip")
+        .append("div")
+        .attr('id', 'tipLine')
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .style("position", "absolute")
+
+    function mouseOver(d) {
+        console.log(d)
+        tooltip
+            .transition()
+            .duration(10)
+            .style("opacity", 1);
+        tooltip
+            .html("Vaccinated: " + d.Vaccine.toLocaleString() + "<br>Death: " + d.Death.toLocaleString())
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 90) + "px")
+    }
+
+    function mouseLeave(d) {
+        tooltip
+            .transition()
+            .duration(100)
+            .style("opacity", 0)
+            .style('pointer-events', 'none');
+    }
 }
